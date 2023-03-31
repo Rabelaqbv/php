779cc2f76579e5eebc15517505ec3e4c06f23e35
Rabelaqbv <125407240+Rabelaqbv@users.noreply.github.com>
2023-03-31 17:14:32 -0300

    1

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+$servername =" localhost ";
+$username=" id20491802_uso ";
+$senha =" rabelo1234Isa> ";
+$nomedb =" id20491802_mldooo ";
+//cria conexão
+$conn = new mysqli( $servername , $username , $senha, $nomedb );
+//testa conexão
+if ( $conn -> connect_error ){
+    die(" conexão com db falhou: ". $conn -> connect_error );
+}
+echo " conectado ao db ";
+?>
---
+++ coontato.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html >
+<html  lang =" en " >
+<body>
+    <meta  charset =" UTF-8 " >
+    <meta  http-equiv =" X-UA-Compatible " content =" IE=edge " >
+    <meta  name =" janela de visualização " conteúdo =" largura=largura do dispositivo, escala inicial=1,0 " >
+    <link  rel =" folha de estilo " href =" estilo.css " >
+    <title > GIOVANNA </ title >
+</body >
+<body >
+    <h1 > Inserir contato </h1 >
+    <form  action =" contato.php " method =" POST " >
+        <label > Nome: </label > <br >
+        <input  type =" text " name =" fnome " >  <br >
+        <label > Sobrenome: </label > <br >
+        <input  type =" text " name =" fsobrenome " > <br >
+        <label > Telefone: </label > <br >
+        <input  type =" text " name =" ftelefone " > <br >
+        <tipo de entrada  =" enviar " valor =" Gravar " >
+</forma> _ _
+<a href =" index.php " > Consultar agenda </a >
+<?php 
+include " conexao.php ";
+?>
+<?php
+if ( $_SERVER [" REQUEST_METHOD "]==" POST "){
+    //Coleta Dados do formulario
+    $nome = $_POST [" fnome "];
+    $sobrenome = $_POST [" fsobrenome "];
+    $telefone = $_POST [" ftelefone "];
+    //inserir dados na tabela
+    $sql =" INSERT into agenda(nome, sobrenome, telefone) VALUES(' $ nome ', ' $ sobrenome ', ' $ telefone ') ";
+    if ( $conn -> query ( $sql )=== TRUE ){
+        echo " Dados inseridos com sucesso "." <br> ";
+    } senão {
+        echo " Erro ao inserir os dados: ". $conn -> erro ;
+    }
+}
+$conn -> fechar ();
+
+?>
+
+
+</body> >
+</html>
