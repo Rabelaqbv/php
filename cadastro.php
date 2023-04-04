aee0993415db4959871dbfa7b5379eef8c52d0a3
Rabelaqbv <125407240+Rabelaqbv@users.noreply.github.com>
2023-04-04 15:55:54 -0300

    fsfgved

---
+++ cadastro.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro agenda</title>
+</head>
+<body>
+    <h1>Cadastro Agenda</h1>
+    <form action="" method="post">
+  <label>Nome: </label><br>
+  <input type="text" name="nome"><br>
+  <label>Sobrenome: </label><br>
+  <input type="text" name="sobrenome"><br>
+  <label>Telefone: </label><br>
+  <input type="text" name="telefone"><br>
+  <input type="submit" value="Gravar">
+</form>
+
+<?php
+include "conexao.php";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+  $sobrenome = $_POST["sobrenome"];
+  $telefone = $_POST["telefone"];
+  $sql = "INSERT INTO agenda (nome, sobrenome, telefone) VALUES ('$nome', '$sobrenome', '$telefone')";
+  if ($conn->query($sql) === TRUE) {
+    echo "Dados inseridos com sucesso"."<br>";
+  } else {
+    echo "Erro ao inserir dados: " . $conn->error;
+  }
+}
+$conn->close();
+?>
+
+
+    <a href="index.php">Exibir Agenda</a>
+    
+</body>
+</html>
